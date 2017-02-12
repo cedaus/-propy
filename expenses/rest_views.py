49cc7589b99e6310d6ab6068ab7ab9161ab47c8a
Saksham <cedaus97@gmail.com>
2017-02-12 16:31:16 +0530

    Year Expense Chart

--- expenses/rest_views.py
+++ expenses/rest_views.py
@@ -5,29 +5,40 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 import models, serializers
-from mysite.utils import get_value_or_404, get_boolean, get_date_obj
+from mysite.utils import get_value_or_404, get_boolean, get_date_obj, get_month_name
 from .utils import get_month_total, get_month_avg, get_month_daily_data, get_month_important, get_month_extra, get_month_tags
 
 class ExpenseReport(APIView):
     def get(self, request, format=None):
         year = int(get_value_or_404(request.GET, 'year'))
         month = int(get_value_or_404(request.GET, 'month'))
+        month_name = get_month_name(month)
         month_total = get_month_total(year, month)
         month_avg = get_month_avg(year, month)
         month_important = get_month_important(year, month)
         month_extra = get_month_extra(year, month)
         month_tags = get_month_tags(year, month)
+        #Find month daily data
         month_daily_data = get_month_daily_data(year, month)
         for i, item in enumerate(month_daily_data):
             sdata = serializers.ExpenseSerializer(item['expenses'], many=True).data
             month_daily_data[i]['expenses'] = sdata
+        #Find a list of total expense of months
+        year_monthly_total = []
+        for i in range(12):
+            year_monthly_total.append(get_month_total(year, i+1))
+        #Bundle all the values
         data = {
+        'year': year,
+        'month': month,
+        'month_name': month_name,
         'month_total': month_total,
         'month_avg': month_avg,
         'month_important':  month_important,
         'month_extra': month_extra,
         'month_tags': month_tags,
-        'month_daily_data':  month_daily_data
+        'month_daily_data':  month_daily_data,
+        'year_monthly_total': year_monthly_total,
         }
         return Response(data, status=status.HTTP_200_OK)
 
